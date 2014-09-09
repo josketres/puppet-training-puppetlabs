8d189342c9fb8e1d1f357a035f767d2046652ce0
josue <josue@puppetlabs.vm>
2014-09-09 14:59:18 +0200

    add message to notify

--- site.pp
+++ site.pp
@@ -39,6 +39,8 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hello ${fqdn}": }
+  notify { "Hello ${fqdn}": 
+     message => 'my message'
+  }
 }
 
