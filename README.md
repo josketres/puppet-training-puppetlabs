662f3a6446674a965d72c9991c994ea65ae289c6
josketres <josketres@gmail.com>
2014-09-10 15:59:05 +0200

    Lab-10.5 - Multi-Platform Apache.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 puppet-training-puppetlabs
 ==========================
+
+* to validate a file
+
+    puppet parser validate path/to/file.pp
+    # should be 0
+    echo $?
--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -1,15 +1,33 @@
 class apache {
 
-  $conf_file = '/etc/httpd/conf/httpd.conf'
+  case $::osfamily {
+    'RedHat': {
+      $conf_file = '/etc/httpd/conf/httpd.conf'
+      $owner = 'apache'
+      $group = 'apache'
+      $service_name = 'httpd'
+      $package_name = 'httpd'
+    }
+    'Debian' : {
+      $conf_file = '/etc/apache2/apache2.conf'
+      $owner = 'www-data'
+      $group = 'www-data'
+      $service_name = 'apache2'
+      $package_name = 'apache2'
+    }
+    default : {
+      fail("osfamily '${::osfamily}' not supported")
+    }
+  }
 
   # set defaults for all file resources in this class
   File {
-    owner  => 'apache',
-    group  => 'apache',
+    owner  => $owner,
+    group  => $group,
     mode   => '0644',
   }
 
-  package { 'httpd':
+  package { $package_name :
     ensure => installed,
   }
 
@@ -28,7 +46,7 @@ class apache {
     source => 'puppet:///modules/apache/index.html',
   }
   
-  service { 'httpd':
+  service { $service_name :
     ensure  => running,
     subscribe => File[$conf_file],
   }
